7a7e1ddbe9a0e56140c2776534bcb15ca91eaa80
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-05-25 13:20:46 -0400

    debugging

--- gdb.sh
+++ gdb.sh
@@ -1,8 +1,8 @@
 v='print *v'
-args=$(./cmds2 "print attributes.size(); $v; c; $v; c; $v; c; $v; c; $v; c")
+args=$(./cmds2 "b _close; run; print attributes.size(); $v; c; $v; c; $v; c; $v; c; $v; c; fin; print buf; print rec; print attributes")
 #args=$(eval echo $args)
 IFS=$'\n' arr=( $(xargs -n1 <<<"$args") ) # https://stackoverflow.com/questions/37372225/convert-a-string-into-an-array-with-bash-honoring-quotes-for-grouping
 #exit
 #batch=--batch
 # https://stackoverflow.com/questions/8657648/how-to-have-gdb-exit-if-program-succeeds-break-if-program-crashes
-sudo runuser -u my_qemu_upstairs_pc_sim -- gdb $batch -x init.txt -ex 'b _close' -ex 'run' ${arr[@]} -ex 'fin' -ex 'print buf' -ex 'print rec' -ex 'print attributes' --args ./a.out /dev/loop3
+sudo runuser -u my_qemu_upstairs_pc_sim -- gdb $batch -x init.txt ${arr[@]} --args ./a.out /dev/loop3
